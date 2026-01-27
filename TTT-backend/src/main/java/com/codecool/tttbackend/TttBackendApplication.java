5a47a6ada147894dddcd3849cb1b364f6525b6f4
Kondor Mark <kondormarkk@gmail.com>
2026-01-27 15:33:47 +0100

    Backend

---
+++ TTT-backend/src/main/java/com/codecool/tttbackend/TttBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.tttbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TttBackendApplication.class, args);
+    }
+
+}
