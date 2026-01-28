52814dde8e263c8c152c04daca99a37ba8fa5353
Doma <makk.domonkos@gmail.com>
2026-01-28 11:18:26 +0100

    SpringBoot environment is up. application properties : added h2 datasource, driverClassname, username, password, serverPort. Pom : added dependencies WebConfiguration : implements WebMvcConfigurer

--- TTT-backend/src/main/java/com/codecool/tttbackend/controller/UserController.java
+++ TTT-backend/src/main/java/com/codecool/tttbackend/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.codecool.tttbackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    @GetMapping("/status")
+    public Map<String, String> getStatus() {
+        return Map.of("status", "OK", "message", "Backend is running on Java 25!");
+    }
 }
